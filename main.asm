d563a0390883c31a993fa031c810383c91b901be
neveroyatniy-bobr <nicant2007@gmail.com>
2026-03-07 03:11:01 +0300

    Добавил процедуру hash_func которая преобразует пароль в аргументах командной строки в хэш

--- main.asm
+++ main.asm
@@ -4,9 +4,7 @@ locals @@
 org 100h
 
 start proc
-    call correct_password_proc
-
-    call wrong_password_proc
+    call hash_func
 
     mov ax, 4C00h
     int 21h
@@ -34,7 +32,32 @@ wrong_password_proc proc
     ret
 endp
 
+hash_func proc
+    push cx ax bx
+
+    mov bx, 82h
+    mov cl, 1
+    jmp @@loop_symbol_cond
+    @@loop_symbol:
+        mov al, cs:[bx]
+        xor al, cs:[hash_const]
+        add al, cl
+        mov cs:[bx], al
+        
+        inc bx
+        inc cl
+    @@loop_symbol_cond:
+        cmp cl, cs:80h
+        jl @@loop_symbol
+
+    pop bx ax cx
+
+    ret
+endp
+
 correct_password_msg db "ai tigr", 0ah, "$"
-  wrong_password_msg    db "nonono" , 0ah, "$"
+  wrong_password_msg db "nonono" , 0ah, "$"
+
+hash_const db 67h
 
 end start
