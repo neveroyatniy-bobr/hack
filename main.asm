0450bf6b5f418885592107e70611eb9ca36bb825
neveroyatniy-bobr <nicant2007@gmail.com>
2026-03-15 03:43:25 +0300

    доделал

--- main.asm
+++ main.asm
@@ -13,6 +13,10 @@ start proc
     call wrong_password_proc
     jmp @@enfif
     @@if_correct:
+    mov si, offset start_decode
+    mov di, offset end_decode
+    call decode_code
+
     call correct_password_proc
     @@enfif:
 
@@ -20,24 +24,69 @@ start proc
     int 21h
 endp
 
-correct_password_proc proc
-    ;Вывожу сообщение о том что пароль правильный
-    mov ah, 09h
-    push cs
-    pop ds
-    mov dx, offset correct_password_msg
-    int 21h
+decode_code proc
+    push ax bx cx bp
 
+    mov bx, si
+    mov cx, 0
+    jmp @@loop_symbol_cond
+    @@loop_symbol:
+        mov al, cs:[82h + bp]
+        xor cs:[bx], al
+
+        inc bx
+        inc bp
+
+        inc bp
+        mov cl, cs:[80h]
+        cmp bp, cx
+        jne @@no_end_pass
+            mov bp, 1
+        @@no_end_pass:
+        dec bp
+    @@loop_symbol_cond:
+        cmp bx, di
+        jl @@loop_symbol
+
+    pop bp cx bx ax
     ret
 endp
 
+correct_password_proc proc
+    start_decode:
+    db  48h, 0eh,  72h, 0b3h, 02h, 16h, 0fh,  9ah
+    db 0d7h, 0ah, 0d5h,  31h, 7ah, 18h, 53h, 0dbh
+    end_decode:
+endp
+
+;-----------------------
+;закодированный кусок
+;push ax ds dx
+
+;Вывожу сообщение о том что пароль правильный
+;mov ah, 09h
+;push cs
+;pop ds
+;mov dx, offset correct_password_msg
+;int 21h
+
+;pop dx ds ax
+
+;ret
+
+;------------
+
 wrong_password_proc proc
+    push ax ds dx
+
     ;Вывожу сообщение о том что пароль неправильный
     mov ah, 09h
     push cs
     pop ds
     mov dx, offset wrong_password_msg
     int 21h
+
+    pop dx ds dx
 
     ret
 endp
@@ -105,7 +154,7 @@ correct_password_msg db "ai tigr", 0ah, "$"
   wrong_password_msg db "nonono" , 0ah, "$"
 
       hash_const db 67h
-correct_hash_len db 10
-    correct_hash db 10h, 14h, 11h, 13h, 17h, 14h, 16h, 1ah, 17h
+correct_hash_len db 6
+    correct_hash db 18h, 10h, 20h, 07h, 0bh
 
 end start
